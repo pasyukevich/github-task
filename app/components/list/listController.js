791fcc3a1099247a1d4f5dd0b37c6c81916792ac
pasyukevich <pasyukevich@live.com>
2018-02-05 11:52:25 +0300

    change to ng-switch

--- app/components/list/listController.js
+++ app/components/list/listController.js
@@ -1,7 +1,9 @@
 angular.module('githubSearch').controller('listController', ($scope, githubSearchFactory, dataFactory, $stateParams, $state) => {
-    const SIZE_OF_PAGE=30;
+    const SIZE_OF_PAGE = 30;
     let currentPage = $stateParams.page || 1,
         currentList = githubSearchFactory.getCurrentListName();
+
+    $scope.currentListName = githubSearchFactory.getCurrentListName();
 
     $scope.itemsPerPage = SIZE_OF_PAGE;
     $scope.currentPage = currentPage;
@@ -14,21 +16,18 @@ angular.module('githubSearch').controller('listController', ($scope, githubSearc
 
 
     $scope.nextPage = function () {
-            $state.go('main.results', {
-                query: $stateParams.query,
-                page: $scope.currentPage > 1 ? $scope.currentPage : ''
-            }, {
-                reload: true
-            });
-        },
-        $scope.isActive = function (checkList) {
-            return githubSearchFactory.getCurrentListName() === checkList;
-        }
+        $state.go('main.results', {
+            query: $stateParams.query,
+            page: $scope.currentPage > 1 ? $scope.currentPage : ''
+        }, {
+            reload: true
+        });
+    }
 
 
     $scope.propertyName = 'forks';
     $scope.reverse = true;
-    
+
     $scope.sortBy = function (propertyName) {
         $scope.reverse = ($scope.propertyName === propertyName) ? !$scope.reverse : false;
         $scope.propertyName = propertyName;
