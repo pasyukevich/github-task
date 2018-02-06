710b91c85ae73d2d96fee64142a8e25241e331f7
pasyukevich <pasyukevich@live.com>
2018-02-06 10:48:45 +0300

    code refactoring

--- app/shared/factories/dataFactory.js
+++ app/shared/factories/dataFactory.js
@@ -32,21 +32,6 @@ angular.module('githubSearch').factory('dataFactory', () => {
                 amount = cache[field].total > MAX_AMOUNT ? MAX_AMOUNT : cache[field].total;
                 this.setData(field, 'amount', amount);
             }
-            // this.setData('users','1',usersData.items);
-            // this.setData('users','total',usersData.total_count);
-            // this.setData('users','amount',usersData.items);
-            //   cache.users[1] = usersData.items;
-            //    cache.users.total = usersData.total_count;
-            // cache.users.amount = cache.users.total > MAX_AMOUNT ? MAX_AMOUNT : cache.users.total;
-            // cache.repositories[1] = reposData.items;
-            // cache.repositories.total = reposData.total_count;
-            // cache.repositories.amount = cache.repositories.total > MAX_AMOUNT ? MAX_AMOUNT : cache.repositories.total;
-            // cache.issues[1] = issuesData.items;
-            // cache.issues.total = issuesData.total_count;
-            // cache.issues.amount = cache.issues.total > MAX_AMOUNT ? MAX_AMOUNT : cache.issues.total;
-            // cache.code[1] = codeData.items;
-            // cache.code.total = codeData.total_count;
-            // cache.code.amount = cache.code.total > MAX_AMOUNT ? MAX_AMOUNT : cache.code.total;
         },
         getData(field, page) {
             return cache[field][page];
