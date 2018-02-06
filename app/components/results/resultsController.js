7518cfecd53dfcfb883ced0603d8902b4c917657
pasyukevich <pasyukevich@live.com>
2018-02-06 11:36:25 +0300

    add loading for new request

--- app/components/results/resultsController.js
+++ app/components/results/resultsController.js
@@ -1,18 +1,22 @@
-angular.module('githubSearch').controller('resultsController', ($scope,items, githubSearchFactory, dataFactory, $stateParams, $state) => {
-    let query = $stateParams.query;
+angular.module('githubSearch').controller('resultsController', ($scope, items, githubSearchFactory, dataFactory, $stateParams, $state) => {
+    let query = $stateParams.query;       
 
-    $scope.items=items;
+    $scope.items = items;
+    $scope.loading=true;
+     
+    if (query===undefined)  $state.go('main');
 
     githubSearchFactory.getSearchResult(query).then(response => {
-        if (response.promiseStatus[0]) {
-            dataFactory.setAll(response.users.data, response.repos.data, 
-                               response.issues.data, response.code.data);
+        if (response.promiseStatus[0]) {                                
+            dataFactory.setAll(response.users.data, response.repos.data,
+                response.issues.data, response.code.data);
         }
         $scope.usersAmount = dataFactory.getItemsTotal('users');
         $scope.reposAmount = dataFactory.getItemsTotal('repositories');
         $scope.issuesAmount = dataFactory.getItemsTotal('issues');
         $scope.codeAmount = dataFactory.getItemsTotal('code');
         $state.go('.list');
+        $scope.loading = false;
     })
 
     $scope.setTo = function (ListName) {
